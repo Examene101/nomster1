53096726fa23144a15fb3fd681b9e849f69e5c62
Jeffruy Examene <Examene7007@gmail.com>
2016-02-14 07:05:15 +0000

    Added validation for information in simpleform

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
      before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	  def index
-		@places = Place.paginate(:page => params[:page], :per_page => 5)
+		  @places = Place.paginate(:page => params[:page], :per_page => 5)
     end
 
     def new
@@ -11,35 +11,49 @@ class PlacesController < ApplicationController
     end
 
     def create
-    	current_user.places.create(place_params)
-    	redirect_to root_path
+    	@place = current_user.places.create(place_params)
+      if @place.valid?
+    	 redirect_to root_path
+      else 
+       render :new, staus: :unprocessable_entity
+      end
     end
 
     def show
-     @place = Place.find(params[:id])
+      @place = Place.find(params[:id])
     end
      
     #action to find edit places to edit
     def edit
-     @place = Place.find(params[:id])
+      @place = Place.find(params[:id])
+       
       if @place.user != current_user
-         return render text: 'Not Allowed', status: :forbidden
+        return render text: 'Not Allowed', status: :forbidden
       end
     end
      
      #action to update places
     def update
       @place = Place.find(params[:id])
-      @place.update_attributes(place_params)
+      
+      if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+      end
+
+     @place.update_attributes(place_params)
+     if @place.valid?
       redirect_to root_path
+     else
+      render :edit, status: :unprocessable_entity
+     end
     end
      
      #delete places action
     def destroy
      @place = Place.find(params[:id])
-      if @place.user != current_user
-        return render text: 'Not Allowed', status: :forbidden
-      end
+     if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+     end
       
       @place.destroy
       redirect_to root_path
