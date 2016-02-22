d4af4066ce5c2be3623adbc3f56ffc56e66513a3
Jeffruy Examene <Examene7007@gmail.com>
2016-02-22 22:05:06 +0000

    Triggering email when comments added

--- db/migrate/20160222012038_create_photos.rb
+++ db/migrate/20160222012038_create_photos.rb
@@ -2,7 +2,7 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
 
-       text :caption
+       t.text :caption
        t.integer :place_id
 
       t.timestamps
